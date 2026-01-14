bfdbf5c00252f5196b3b45f2efc2fd498c8c0bd8
Grace Teng <pelicularities@users.noreply.github.com>
2026-01-15 05:41:04 +0800

    [Neetcode/Java] Refactor ValidParentheses

--- app/src/main/java/me/graceteng/neetcode/ValidParentheses.java
+++ app/src/main/java/me/graceteng/neetcode/ValidParentheses.java
@@ -7,43 +7,42 @@ public class ValidParentheses {
     static boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (currentChar == '('
-                || currentChar == '['
-                || currentChar == '{') {
+            char currentParens = s.charAt(i);
+            // check for open parens
+            if (isOpen(currentParens)) {
                 // push open parens on stack
-                stack.push(currentChar);
+                stack.push(currentParens);
                 continue;
             }
+
             // since the current char is a closing parens,
             // check if the stack is empty
             if (stack.isEmpty()) {
                 return false;
             }
 
-            // pop closing parens off the stack
+            // pop open parens off the stack
             // and make sure it matches
-            if (currentChar == ')') {
-                Character pop = stack.pop();
-                if (!pop.equals('(')) {
-                    return false;
-                }
-            }
-            if (currentChar == '}') {
-                Character pop = stack.pop();
-                if (!pop.equals('{')) {
-                    return false;
-                }
-            }
-            if (currentChar == ']') {
-                Character pop = stack.pop();
-                if (!pop.equals('[')) {
-                    return false;
-                }
+            Character lastParens = stack.pop();
+            if (lastParens != getMatchingOpenParens(currentParens)) {
+                return false;
             }
         }
         // if stack is not empty,
         // there was an unclosed parens
         return stack.isEmpty();
+    }
+
+    static boolean isOpen(char c) {
+        return (c == '(' || c == '[' || c == '{');
+    }
+
+    static char getMatchingOpenParens(char closingParens) {
+        return switch (closingParens) {
+            case ')' -> '(';
+            case ']' -> '[';
+            case '}' -> '{';
+            default -> throw new IllegalArgumentException("Argument must be one of ')', ']' or '}'");
+        };
     }
 }
